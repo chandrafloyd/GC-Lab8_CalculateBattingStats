6b213cab6ea3866e29e4e94c5e782c9b37620efe
chandrafloyd <chandra_floyd@yahoo.com>
2018-02-01 07:21:23 -0500

    Updated calculations and created separate class for methods

--- Lab8-BattingAverageCalculator/Program.cs
+++ Lab8-BattingAverageCalculator/Program.cs
@@ -5,108 +5,121 @@ using System.Text;
 
 namespace Lab8_BattingAverageCalculator
 {
+
+    class Methods
+    {
+        //METHOD TO CALCULATE AVERAGES
+        public static float CalculateAverage(float sum1, int bases)
+        {
+            return sum1 / bases;
+
+        }
+
+
+        // METHOD TO END THE GAME
+        //String Continue, EndGame;
+
+        public static bool EndTheGame(EndGame, Continue)
+        {
+           
+            if (Continue == "N")
+            {
+                EndGame = true;  //set EndGame to true in order to exit
+                Console.WriteLine("Thanks for playing! Goodbye!");
+            }
+        }
+
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //MAIN METHOD
         {
 
-            //formulas:
-            //Batting Average =  Non-Zero At Bats / Total # At Bats 
-            //Slugging Percentage = Total # of Bases Earned / Total # At Bats
+            bool EndGame = false;  //start with EndGame set to false in order to play the game
 
-
-            float BattingAverage;
-            float SluggingPercentage;
-            float[] AtBatResults = { 0, 1, 2, 3, 4 };  //TO DO - validate so that only these 5 choices can be entered 
-            
-            //GET NUMBER OF PLAYERS
-            Console.WriteLine("Please enter number of players:");
-            int NumberOfPlayers = int.Parse(Console.ReadLine());
-
-            //GET NUMBER OF AT BATS PER PLAYER
-            Console.WriteLine("Please enter number of times at-bats for each player");
-            int NumberOfAtBats = int.Parse(Console.ReadLine());
-
-            //ARRAY TO HOLD AT EACH PLAYERS AT-BAT STATS 
-            float[,] BattingStats = new float[NumberOfPlayers, NumberOfAtBats];
-
-
-            //GET THE NUMBER PLAYERS AND ALL OF THEIR AT BATS
-
-            for (int Row = 0; Row < NumberOfPlayers; Row++)  //for each player....
+            while (EndGame == false)
             {
-                Console.WriteLine($"Please enter " + NumberOfPlayers + " Batting Results (press enter after each entry) for each player {Row + 1}");
+                //GET NUMBER OF PLAYERS
+                Console.WriteLine("Please enter number of players: \n");
+                int NumberOfPlayers = int.Parse(Console.ReadLine());
 
-                for (int Column= 0; Column < NumberOfAtBats; Column++)  //....add this many at bat stats
+
+                //GET NUMBER OF AT BATS PER PLAYER
+                Console.WriteLine("Please enter number of at-bats for each player \n");
+                int NumberOfAtBats = int.Parse(Console.ReadLine());
+
+                //ARRAY TO HOLD AT EACH PLAYER AND THEIR AT-BAT STATS 
+                float[,] BattingStats = new float[NumberOfPlayers, NumberOfAtBats];
+
+
+                //READ THE NUMBER PLAYERS AND ALL OF THEIR AT BATS
+
+                for (int Row = 0; Row < NumberOfPlayers; Row++)  //for each player....
                 {
-                    BattingStats[Row, Column] = float.Parse(Console.ReadLine()); //this collects the information for the array and puts the player in the row and the stats in the column
-                }
-            }
-             
 
+                    Console.WriteLine($"Please enter number of at-bats for player[{Row + 1}]\n");       //get number of at bats per player
 
-            for (int Row = 0; Row < NumberOfPlayers; Row++)
-            {
-                Console.WriteLine($"Enter the number of grades for student {i + 1}");
-                int Columns = int.Parse(Console.ReadLine());
-            }
+                    NumberOfPlayers = int.Parse(Console.ReadLine());
 
-            StudentGradeSheet[i] = new int[Columns];
+                    for (int Column = 0; Column < NumberOfAtBats; Column++)  //....add this many at bat stats
 
-            //second array/loop needed (modify)
-            for (int Row = 0; Row < NumberOfStudents; Row++)
-            {
-                for (int Column = 0; Column < StudentGradeSheet[Row].Length; Column++)
-                {
-                  //  StudentGradeSheet[Row][Column] = int.Parse....//what goes here
-                }
-            }
+                    {
+                        float[] AtBatResults = { 0, 1, 2, 3, 4 };  //TO DO - validate so that only these 5 choices can be entered 
 
+                        BattingStats[Row, Column] = float.Parse(Console.ReadLine()); //this collects the information for the array and puts the player in the row and the stats in the column
+                    }
 
-            //need a 3rd array/loop to do the calculations (modify)
-            for (int Row = 0; Row < NumberOfStudents; Row++)
-            {
-                float Sum = 0; //set sum to 0 becuase each student has their own sum. Start with 0 and add each grade to the 0. When it goes into the 2nd loop, it will add the grades from the prior student unless you re-set it to 0
-
-                for (int Column = 0; Column < NumberOfGrades; Column++)
-                {
-                    Sum = Sum + StudentGradeSheet[Row, Column];
                 }
 
-                Console.WriteLine($"Letter grade for student {Row + 1} is {CalculateLetterGrade(Sum)}");
+                //formulas:
+                //Batting Average =  Non-Zero At Bats / Total # At Bats 
+                //Slugging Percentage = Total # of Bases Earned / Total # At Bats
+
+                for (int Row = 0; Row < NumberOfPlayers; Row++)
+                {
+                    float Sum1 = 0; //set sum to 0 bc each batter has their own sum. Start with 0 and add each at bat to the 0. When it goes into the 2nd loop, it will add the at bats from the prior player unless you re-set it to 0
+                    int Count = 0;
+
+                    for (int Column = 0; Column < NumberOfAtBats; Column++)
+                    {
+                        Sum1 += BattingStats[Row, Column];
+                        if (BattingStats[Row, Column] > 0) //this skips the 0s
+                        {
+                            Count++;   //this only counts the nonzero at bats
+                        }
+
+                    }
+                    Console.WriteLine($"Player{Row + 1} results are");
+                    Console.WriteLine(new string('*', 20));
+                    Console.WriteLine($"The slugging average for Player{Row + 1} is {CalculateAverage(Sum1, NumberOfAtBats)}\n");
+                    Console.WriteLine($"The batting average for Player{Row + 1} is {CalculateAverage(Count, NumberOfAtBats)}\n");
+
+
+                }
+
+                //PLAY OR END GAME?
+
+
+                Console.WriteLine("Would you like to enter more stats? Y/N");
+                string Continue = Console.ReadLine();
+                if (Continue == "N")
+                {
+                    EndGame = true;
+                    EndTheGame();
+                }
 
             }
-
-
-
         }
 
-
-        //CONTINUE METHOD
-        public static string Continue()
+        private static object CalculateAverage(float sum1, int numberOfAtBats)
         {
+            throw new NotImplementedException();
         }
-
-        //ENDGAME METHOD
-        public static string EndGame()
-        {
-        }
-
-
-        ////PLAY OR END GAME?
-        //bool EndGame = false;  //start with EndGame set to false in order to play the game
-
-        //Console.WriteLine("Would you like to roll the dice? Y/N ");
-        //        string RollDice = Console.ReadLine();
-
-
-        //    //DON'T ROLL THE DICE - END GAME
-        //    if (RollDice == "N")
-        //    {
-        //        EndGame = true;  //set EndGame to true in order to exit
-        //        Console.WriteLine("Thanks for playing! Goodbye!");
-        //    }
-
-
+    }
+    }
 }
-}
+
+
+
+
